3cc449c146f80e4a89ad7f8767091d62816893b1
mdalamin706688 <mdalamin706688@gmail.com>
2021-02-05 18:29:59 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <comp-header/>
+      <div>
+        <comp-featured/>
+        <comp-menu/>
+        <comp-call-us/>
+        <comp-newsletter/>
+      </div>
+      <comp-footer/>
+  </div>
+</template>
+
+
+<script>
+  import compHeader from './components/Header-footer/Header.vue';
+  import compFooter from './components/Header-footer/Footer.vue';
+  import compFeatured from './components/Featured/index.vue';
+  import compMenu from './components/Menu/index.vue'
+  import compCallUs from './components/CallUs/index.vue';
+  import compNewsletter from './components/Newsletter/index.vue';
+
+  export default {
+    components:{
+      compHeader,
+      compFooter,
+      compFeatured,
+      compMenu,
+      compCallUs,
+      compNewsletter
+    }
+  }
+</script>
+
+<style>
+  @import './assets/style.css';
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource';
+import VueScrollReveal from 'vue-scroll-reveal';
+
+import { MdButton, MdDrawer,MdCard } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueResource)
+
+Vue.use(MdButton);
+Vue.use(MdDrawer);
+Vue.use(MdCard);
+
+Vue.use(VueScrollReveal,{
+  duration:800,
+  scale:1,
+  distance:'50px'
+});
+
+Vue.http.options.root = 'https://domnoo-c81fd.firebaseio.com/'
+
+new Vue({
+  el:'#app',
+  render: h => h(App)
+})
